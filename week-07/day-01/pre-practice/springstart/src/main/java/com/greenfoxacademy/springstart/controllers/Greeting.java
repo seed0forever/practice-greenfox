7b3f92358d153aa849b4285aa66fe3a461c31cdb
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-28 20:41:23 +0200

    w07d01pre springstart add first static and dynamic Hello World web apps

---
+++ week-07/day-01/pre-practice/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class Greeting {
+
+  @Getter private final long id;
+  @Getter private final String content;
+
+}
