571747c60869c5a2a718a9ebe440006328d61597
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-01 15:56:26 +0200

    w07d01pre springstart: add HelloAll initial version

---
+++ week-07/day-01/pre-practice/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloAllWebController.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloAllWebController {
+  private final static String[] HELLOS = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  @RequestMapping("/web/greeting-all")
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Model model){
+    model.addAttribute("name", name);
+    model.addAttribute("helloElements", HELLOS);
+    return "hello-all";
+  }
+}
