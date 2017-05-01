24af33e357152a485023efd2d353788e453cf42f
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-01 16:54:46 +0200

    w07d01pre springstart: add HelloAll color randomizer

--- week-07/day-01/pre-practice/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloAllWebController.java
+++ week-07/day-01/pre-practice/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloAllWebController.java
@@ -7,15 +7,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloAllWebController {
+
   private final static String[] HELLOS = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
           "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
           "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
           "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
+
   @RequestMapping("/web/greeting-all")
-  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Model model){
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+          Model model) {
     model.addAttribute("name", name);
     model.addAttribute("helloElements", HELLOS);
+
+    String[] randomHelloColors = fillWithRandomColors(HELLOS.length);
+    model.addAttribute("color", randomHelloColors);
+
     return "hello-all";
+  }
+
+  private String[] fillWithRandomColors(int numberOfColors) {
+    String[] colorArray = new String[numberOfColors];
+    for (int i = 0; i < numberOfColors; i++) {
+      colorArray[i] = generateRandomColorRgb();
+    }
+    return colorArray;
+  }
+
+  private String generateRandomColorRgb() {
+    String rgbFormat = "rgb(%d,%d,%d)";
+    int red = (int) (Math.random() * 200);
+    int green = (int) (Math.random() * 200);
+    int blue = (int) (Math.random() * 200);
+    return String.format(rgbFormat, red, green, blue);
   }
 }
